0b0debfb3ea997db009f05aeab972c00f736265f
Jitendra Marndi <masterjeet@masterjeet.com>
2017-06-19 01:00:45 +0530

    Test for position

--- test/src/position_unittest.cpp
+++ test/src/position_unittest.cpp
@@ -21,9 +21,32 @@ TEST_F(PositionTest, constructors) {
     std::stringstream ss;
     std::string pos;
 
+    //Initialization with single FaceSide
     ss << pf;
     std::getline(ss, pos);
     EXPECT_EQ("Position: ptype = Center, vecSide = { Front }", pos);
     ss.str("");
     ss.clear();
+
+    //Initialization with double FaceSide
+    ss << pbl;
+    std::getline(ss, pos);
+    EXPECT_EQ("Position: ptype = Edge, vecSide = { Back Left }", pos);
+    ss.str("");
+    ss.clear();
+
+    //Initialization with triple FaceSide
+    ss << pdrf;
+    std::getline(ss, pos);
+    EXPECT_EQ("Position: ptype = Corner, vecSide = { Down Right Front }", pos);
+}
+
+TEST_F(PositionTest, minions) {
+    //getPositionType()
+    EXPECT_EQ(corner, pdrf.getPositionType());
+    //getSideAt()
+    EXPECT_EQ(left, pbl.getSideAt(1));
+    //getVecSide()
+    const std::vector<FaceSide> *pvecSide = pf.getSide();
+    EXPECT_EQ(front, pvecSide->at(0));
 }
