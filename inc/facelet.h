a8fbc1d79c491ede03ffef7db28072df98af686e
Jitendra Marndi <masterjeet@masterjeet.com>
2017-07-04 12:54:35 +0530

    Fixiing

--- inc/facelet.h
+++ inc/facelet.h
@@ -11,54 +11,6 @@
 #include "position.h"
 #include <ostream>
 
-class Facelet {
-    private:
-
-        //Holds face color
-        Color col;
-        FaceSide side;
-    public:
-        //Default constructor
-        Facelet(){};
-
-        //Initialization from Color and FaceSice
-        Facelet(const Color , const FaceSide side);
-        
-        //Initialization from FaceSice and Color
-        Facelet(const FaceSide side, const Color);
-
-        //Returns pointer to 'col'
-        Color getColor() const { return col; }
-        
-        //Returns 'side'
-        FaceSide getFaceSide() const { return side; }
-
-        //Sets 'side'
-        void setFaceSide(const FaceSide _side) { side = _side; }
-
-        //Overloading operator*= to define multiplication of type
-        //Facelet*=FaceSide
-        Facelet& operator*=(const FaceSide& rhs); 
-
-        //Overloading operator* to define multiplication of type
-        //Facelet*FaceSide
-        friend Facelet operator*(Facelet lhs, const FaceSide& rhs){ lhs *= rhs; return lhs; }
-
-        //Equality 
-        friend bool operator==(const Facelet& lhs, const Facelet& rhs) { return  lhs.col == rhs.col && lhs.side == rhs.side; }
-
-        //Write to output stream
-        friend std::ostream& operator<<(std::ostream& os, const Facelet& F){ return os << "Facelet: col = " << F.col << ", side = " << F.side; }
-
-        //Check if given two faclets are opposite of each other
-        friend bool areOpposite(const Facelet& first, const Facelet& second){ return areOpposite(first.side, second.side); }
-
-        //Check if any of given three Facelets contain opposite faces
-        friend bool anyOpposite(const Facelet& first, const Facelet& second, const Facelet& third){ return anyOpposite(first.side, second.side, third.side); }
-};
-
-
-
 class FaceletPosition: public Position  {
     public:
         //All constructor similar to those in base class Position
@@ -75,6 +27,57 @@ class FaceletPosition: public Position  {
         friend bool operator!=(const FaceletPosition& lhs, const FaceletPosition& rhs){ return !(lhs == rhs); }
 
 };
+
+class Facelet {
+    private:
+
+        //Holds face color
+        Color col;
+        FaceletPosition pos;
+    public:
+        //Default constructor
+        Facelet(){};
+
+        //Initialization from Color and FaceSice
+        Facelet(const Color _col, const FaceletPosition _pos):col(_col), pos(_pos) {}
+        
+        //Initialization from FaceSice and Color
+        Facelet(const FaceletPosition _pos, const Color _col): Facelet(_col, _pos) {}
+
+        //Returns pointer to 'col'
+        Color getColor() const { return col; }
+        
+        //Returns of Facelet
+        FaceletPosition getPosition() const { return pos; }
+
+        //Returns which FaceSide given Facelet is located at
+        FaceSide side() const { return pos.getSideAt(0); }
+
+        //Sets 'side'
+        void setPosition(const FaceletPosition _pos) { pos = _pos; }
+
+        //Overloading operator*= to define multiplication of type
+        //Facelet*=FaceSide
+        Facelet& operator*=(const FaceSide& rhs); 
+
+        //Overloading operator* to define multiplication of type
+        //Facelet*FaceSide
+        friend Facelet operator*(Facelet lhs, const FaceSide& rhs){ lhs *= rhs; return lhs; }
+
+        //Equality 
+        friend bool operator==(const Facelet& lhs, const Facelet& rhs) { return  lhs.col == rhs.col && lhs.side() == rhs.side(); }
+
+        //Write to output stream
+        friend std::ostream& operator<<(std::ostream& os, const Facelet& F){ return os << "Facelet: col = " << F.col << ", side = " << F.side(); }
+
+        //Check if given two faclets are opposite of each other
+        friend bool areOpposite(const Facelet& first, const Facelet& second){ return areOpposite(first.side(), second.side()); }
+
+        //Check if any of given three Facelets contain opposite faces
+        friend bool anyOpposite(const Facelet& first, const Facelet& second, const Facelet& third){ return anyOpposite(first.side(), second.side(), third.side()); }
+};
+
+
 
 /* =============== Hash for Position ================
  * This is required so that it can be used as key in
--- src/facelet.cpp
+++ src/facelet.cpp
@@ -7,13 +7,6 @@
 
 #include "facelet.h"
 
-Facelet::Facelet(const Color _col, const FaceSide _side): col(_col), side(_side){
-    //Nothing for now
-}
-
-Facelet::Facelet(const FaceSide _side, const Color _col): Facelet(_col, _side){
-    //Nothing for now
-}
 
 Facelet& Facelet::operator*=(const FaceSide& rhs){
     side *= rhs;
