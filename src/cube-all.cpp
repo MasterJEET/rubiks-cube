be245a36c17962c3cbcb7f842e87e76ce2d55886
MasterJEET <jitendramarndi@gmail.com>
2017-07-10 23:41:45 +0530

    gitignore

---
+++ src/cube-all.cpp
@@ -0,0 +1,14 @@
+/*
+ * @author: MasterJEET
+ * @email : masterjeet9@gmail.com
+ * @date  : 06th July 2017
+ *
+ * */
+
+//Pulling all source files to here
+
+#include "common.cpp"
+#include "position.cpp"
+#include "facelet.cpp"
+#include "cubelet.cpp"
+#include "cube.cpp"
